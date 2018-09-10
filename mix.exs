14ebbcf54fcbf1f3497421ed0b3faf3888b0d17f
Nick Vernij <hello@nickforall.nl>
2018-09-10 20:54:20 +0200

    Lower the elixir version for better compatibility.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Freight.MixProject do
     [
       app: :freight,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
