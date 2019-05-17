909f5297181fd607e1ab9e6dea66a2de4c6ab2f3
Nick Vernij <hello@nickforall.nl>
2019-05-17 13:09:55 +0200

    Add support for custom messages

--- lib/integrations/ecto.ex
+++ lib/integrations/ecto.ex
@@ -13,10 +13,11 @@ defmodule Freight.Integrations.Ecto do
   """
   def convert_error(%Changeset{} = changeset) do
     changeset
-    |> traverse_errors(&traverse_errors_map/1)
-    |> build_error_list()
+    |> traverse_errors(&traverse_errors_map/3)
+    |> build_error_list
   end
 
+  # converts to string without raising
   defp safe_to_string(term) do
     case String.Chars.impl_for(term) do
       nil -> inspect(term)
@@ -25,7 +26,20 @@ defmodule Freight.Integrations.Ecto do
   end
 
   # https://hexdocs.pm/ecto/Ecto.Changeset.html#traverse_errors/2
-  defp traverse_errors_map({msg, opts}) do
+  defp traverse_errors_map(_changeset, field, {msg, opts} = error) do
+    validation = Keyword.get(opts, :validation)
+
+    case is_custom_error_message?(validation, msg) do
+      true ->
+        reduce_options_onto_message(error)
+
+      false ->
+        msg = reduce_options_onto_message(error)
+        Enum.join([Atom.to_string(field), msg], " ")
+    end
+  end
+
+  defp reduce_options_onto_message({msg, opts}) do
     Enum.reduce(opts, msg, fn {key, value}, acc ->
       String.replace(acc, "%{#{key}}", safe_to_string(value))
     end)
@@ -33,8 +47,32 @@ defmodule Freight.Integrations.Ecto do
 
   # maps key-value map where keys are field names and values are errors to a list
   defp build_error_list(map) do
-    Enum.map(map, fn {key, value} ->
-      Enum.join([Atom.to_string(key), value], " ")
-    end)
+    Enum.reduce(map, [], fn {_, value}, acc -> acc ++ value end)
   end
+
+  # to be honest, i don't know if this is the best way to see if there are custom errors.
+  # however i'm already in too deep now.
+  defp is_custom_error_message?(:acceptance, "must be accepted"), do: false
+  defp is_custom_error_message?(:confirmation, "does not match confirmation"), do: false
+  defp is_custom_error_message?(:exclusion, "is reserved"), do: false
+  defp is_custom_error_message?(:format, "has invalid format"), do: false
+  defp is_custom_error_message?(:inclusion, "is invalid"), do: false
+  defp is_custom_error_message?(:length, "should be %{count} character(s)"), do: false
+  defp is_custom_error_message?(:length, "should be %{count} byte(s)"), do: false
+  defp is_custom_error_message?(:length, "should be %{count} item(s)"), do: false
+  defp is_custom_error_message?(:length, "should be at least %{count} character(s)"), do: false
+  defp is_custom_error_message?(:length, "should be at least %{count} byte(s)"), do: false
+  defp is_custom_error_message?(:length, "should be at least %{count} item(s)"), do: false
+  defp is_custom_error_message?(:length, "should be at most %{count} character(s)"), do: false
+  defp is_custom_error_message?(:length, "should be at most %{count} byte(s)"), do: false
+  defp is_custom_error_message?(:length, "should be at most %{count} item(s)"), do: false
+  defp is_custom_error_message?(:number, "must be less than %{number}"), do: false
+  defp is_custom_error_message?(:number, "must be greater than %{number}"), do: false
+  defp is_custom_error_message?(:number, "must be less than or equal to %{number}"), do: false
+  defp is_custom_error_message?(:number, "must be greater than or equal to %{number}"), do: false
+  defp is_custom_error_message?(:number, "must be equal to %{number}"), do: false
+  defp is_custom_error_message?(:number, "must be not equal to %{number}"), do: false
+  defp is_custom_error_message?(:required, "can't be blank"), do: false
+  defp is_custom_error_message?(:subset, "has an invalid entry"), do: false
+  defp is_custom_error_message?(_, _), do: true
 end
